75ddce06457cb60af5b745e459a41f918fd9d6f3
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2017-04-12 20:15:35 +1200

    Add basic markdown mode

---
+++ config/rk-markdown.el
@@ -0,0 +1,24 @@
+;;; rk-markdown.el --- Configuration for Markdown files.  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017 Raghuvir Kasturi
+
+;; Author: Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
+
+;;; Commentary:
+
+;;; Code:
+
+(eval-when-compile
+  (require 'use-package))
+
+(use-package markdown-mode
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+
+  :init (setq markdown-command "multimarkdown"))
+
+(provide 'rk-markdown)
+
+;;; rk-markdown.el ends here
