e8fb5c1716077a101803ba468ba14b126879dd93
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2017-05-23 15:56:36 +1200

    Fix warnings

--- config/rk-evil.el
+++ config/rk-evil.el
@@ -14,6 +14,7 @@
 (require 'spacemacs-keys)
 
 (use-package evil
+  :commands (evil-scroll-up)
   :preface
   (defun rk-evil--sp-delete-and-join-compat (fn &rest args)
     (if (bound-and-true-p smartparens-strict-mode)
@@ -70,7 +71,9 @@
 (use-package evil-surround
   :after evil
   :commands (global-evil-surround-mode
-             evil-surround-region)
+             evil-surround-region
+             evil-surround-read-tag
+             evil-surround-function)
 
   :preface
   (autoload 'evil-substitute "evil-commands")
@@ -112,7 +115,10 @@
     (setq iedit-toggle-key-default nil)
 
     ;; Enable leader key in iedit and iedit-insert states
-    (define-key evil-iedit-state-map (kbd "SPC") spacemacs-keys-default-map)))
+    (define-key evil-iedit-state-map (kbd "SPC") spacemacs-keys-default-map))
+
+  :defines (iedit-current-symbol-default
+            iedit-only-at-symbol-boundaries))
 
 (use-package evil-ex
   :defer t
@@ -146,6 +152,7 @@
 
 (use-package evil-escape
   :after evil
+  :commands (evil-escape-mode)
   :config
   (progn
     (setq-default evil-escape-key-sequence "jk")
