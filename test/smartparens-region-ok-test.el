daa0715b85458ef88e940f40990b07eab3506418
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2017-09-17 13:42:19 +1200

    Squashed 'lisp/smartparens/' changes from 1612d6f72..7d368051d
    7d368051d Merge pull request #789 from antifuchs/rust-match-branches
    830162585 In rust-mode, allow typing match branches (=>) in strict mode
    6db990464 Add support icon
    25508600c Merge pull request #777 from whysoserious/fix-slurp-in-elixir-mode
    15e6a353f Add tests of barfing and slurping in elixir-mode
    53921d698 Fix handling of commas during slurp and barf in elixir-mode
    759d3b40e Add test for sp-backward-delete-char on pair with skip-match
    4899d802f [Fix #775] Fix regions containing newlines or sexp suffixes from never being balanced
    d4445621b Load smartparens-text after loading some text mode
    8987c405f [Fix #713] Support insertion of emoticons in strict mode in text-mode
    c83188b03 sp-region-ok-p now works with all smartparens pairs not just syntax-table
    43c3922f6 [Fix #774] sp-get-thing skips closing delimiters with skip function
    35432d939 sp--strict-regexp-quote returns unmatchable regexp on nil
    05b42a251 [Fix #696] Reindent in js2-mode
    9cff71a5a [Fix #701] js-mode provides js feature
    a32063270 [Fix #678] Unwrap last region in strict mode on DEL with sp-autodelete-wrap
    
    git-subtree-dir: lisp/smartparens
    git-subtree-split: 7d368051dec40e8e129d84bca5070bb7e83ad72a

--- test/smartparens-region-ok-test.el
+++ test/smartparens-region-ok-test.el
@@ -1,8 +1,8 @@
-(defun sp-test--string-valid-p (str)
-  (with-temp-buffer
-    (insert str)
-    (emacs-lisp-mode)
-    (sp-region-ok-p (point-min) (point-max))))
+(defmacro sp-test--string-valid-p (str &optional init)
+  `(sp-test-with-temp-buffer ,str
+       ,(if init `,init '(emacs-lisp-mode))
+     (and (sp-region-ok-p (point-min) (point-max))
+          (sp-region-ok-p (point-max) (point-min)))))
 
 (ert-deftest sp-test-region-ok-unbalanced-paren ()
   (should-not (sp-test--string-valid-p "foo)")))
@@ -10,11 +10,50 @@
 (ert-deftest sp-test-region-ok-unbalanced-string ()
   (should-not (sp-test--string-valid-p "foo\"")))
 
+(ert-deftest sp-test-region-ok-no-sexp ()
+  (should (sp-test--string-valid-p "foo")))
+
 (ert-deftest sp-test-region-ok-balanced-string ()
   (should (sp-test--string-valid-p "\"foo\"")))
 
+(ert-deftest sp-test-region-ok-balanced-string-with-escaped-quote ()
+  (should (sp-test--string-valid-p "\"foo \\\" bar\"")))
+
 (ert-deftest sp-test-region-ok-balanced-parens ()
   (should (sp-test--string-valid-p "(foo)")))
+
+(ert-deftest sp-test-region-ok-balanced-parens-with-newline ()
+  (should (sp-test--string-valid-p "(foo)\n\n")))
+
+(ert-deftest sp-test-region-ok-flipped-parens ()
+  (should-not (sp-test--string-valid-p ")foo(")))
+
+(ert-deftest sp-test-region-ok-c++-mode-no-sexp ()
+  (should (sp-test--string-valid-p "foo;" (c++-mode))))
+
+(ert-deftest sp-test-region-ok-c++-mode-balanced-sexp-with-newline ()
+  (should (sp-test--string-valid-p "{
+    animDivisor = FAWorld::World::getTicksInPeriod(0.1f);
+}" (c++-mode))))
+
+(ert-deftest sp-test-region-ok-c++-mode-balanced-sexp-single-line ()
+  (should (sp-test--string-valid-p "{ animDivisor = FAWorld::World::getTicksInPeriod(0.1f); }" (c++-mode))))
+
+(ert-deftest sp-test-region-ok-c++-mode-unbalanced-sexp-single-line ()
+  (should-not (sp-test--string-valid-p "animDivisor = FAWorld::World::getTicksInPeriod(0.1f); }" (c++-mode))))
+
+(ert-deftest sp-test-region-ok-balanced-parens-with-skip-match ()
+  (let ((sp-pairs '((t . ((:open "(" :close ")"
+                           :actions (insert wrap autoskip navigate)
+                           :skip-match (lambda (ms mb me)
+                                         (save-excursion
+                                           (goto-char mb)
+                                           (sp--looking-back-p "skip" 4)))))))))
+    (should (sp-test--string-valid-p "(fo skip) o)"))))
+
+(ert-deftest sp-test-region-ok-with-non-navigable-pairs ()
+  (let ((sp-pairs '((t . ((:open "*" :close "*" :actions (wrap)))))))
+    (should (sp-test--string-valid-p "*a*"))))
 
 (ert-deftest sp-test-region-ok-with-trailing-garbage ()
   (should (sp-test--string-valid-p "(foo) asdf!$#$^")))
