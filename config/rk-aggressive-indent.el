af4b4d923907f1fd6412e66630cf19556c0dfe0e
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2017-10-19 11:29:19 +1300

    Refactor aggressive indent excluded mode list

--- config/rk-aggressive-indent.el
+++ config/rk-aggressive-indent.el
@@ -8,21 +8,30 @@
 
 ;;; Code:
 
+(defconst rk-aggressive-indent-exclude-modes
+  '(toml-mode
+    diff-auto-refine-mode
+    sql-mode
+    restclient-mode
+    rk-web-js-mode
+    rnc-mode
+    scala-mode)
+  "List of modes to be excluded from aggressive indent.")
+
 (eval-when-compile
   (require 'use-package))
 
 (use-package aggressive-indent
   :commands (global-aggressive-indent-mode)
   :defer 1
+  :preface
+  (defun rk-extend-aggressive-indent-exclude-modes ()
+    "Add select modes to be excluded by aggressive indent."
+    (dolist (item rk-aggressive-indent-exclude-modes)
+      (add-to-list 'aggressive-indent-excluded-modes item)))
   :config
   (progn
-    (add-to-list 'aggressive-indent-excluded-modes 'diff-auto-refine-mode)
-    (add-to-list 'aggressive-indent-excluded-modes 'toml-mode)
-    (add-to-list 'aggressive-indent-excluded-modes 'sql-mode)
-    (add-to-list 'aggressive-indent-excluded-modes 'restclient-mode)
-    (add-to-list 'aggressive-indent-excluded-modes 'rk-web-js-mode)
-    (add-to-list 'aggressive-indent-excluded-modes 'rnc-mode)
-
+    (rk-extend-aggressive-indent-exclude-modes)
     (global-aggressive-indent-mode +1)))
 
 (provide 'rk-aggressive-indent)
