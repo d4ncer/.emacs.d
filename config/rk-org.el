795b0cee0ccd3db9aefa7f28dcd39a7ea36386f2
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2017-10-28 17:55:26 +1300

    Fix org hydra

--- config/rk-org.el
+++ config/rk-org.el
@@ -278,11 +278,10 @@ Do not scheduled items or repeating todos."
 
     (use-package rk-org-agenda-transient-state
       :after org
-      :commands (rk-org-agenda-main-transient-state/body)
+      :commands (rk-org-agenda-hydra/body)
       :config
       (progn
-        (define-key org-agenda-mode-map (kbd "C-.") #'rk-org-agenda-main-transient-state/body)))
-
+        (define-key org-agenda-mode-map (kbd "C-.") #'rk-org-agenda-hydra/body)))
 
     (setq org-agenda-custom-commands
           '(("A" "Agenda and next actions"
