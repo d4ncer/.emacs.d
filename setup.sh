7da3a055082f5437a96ba8d70c3abe33517253e4
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2018-01-18 09:59:50 +0530

    Clean up setup script

--- setup.sh
+++ setup.sh
@@ -5,6 +5,11 @@ set -e
 mv ~/.emacs.d ~/.emacs.d.bak
 git clone git@github.com:d4ncer/.emacs.d.git ~/.emacs.d
 
+# Org setup
+cd ~/.emacs.d/lisp/org-mode
+make
+
+# Go setup
 go get -u -v github.com/nsf/gocode
 go get -u -v github.com/rogpeppe/godef
 go get -u -v github.com/kardianos/govendor
@@ -12,10 +17,8 @@ go get -u -v golang.org/x/tools/cmd/guru
 go get -u -v golang.org/x/tools/cmd/gorename
 go get -u -v golang.org/x/tools/cmd/goimports
 
+# JS setup
 cd ~/.emacs.d/lisp/tern
-
 npm install
-
 npm install -g stylefmt eslint flow prettier eslint-config-airbnb eslint-config-prettier
-
 cp ~/.emacs.d/.eslintrc.json.sample ~/.eslintrc.json
