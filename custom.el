311ecd40ddf8ec175089e4ee748c63ebc173d7a3
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2018-02-08 17:21:56 +1300

    Add dark theme to safe

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+    ("6adde1c066634d9814ee961bab076f1e650c7040e328bf27538118bf3e489024" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(magit-fetch-arguments (quote ("--prune")))
  '(package-selected-packages (quote (base16-theme flycheck))))
 (custom-set-faces
