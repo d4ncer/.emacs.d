aa9a4f7e4b3b99a81a58060d4a680a9e8496a9ef
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2018-03-26 19:43:29 +1300

    Refactor to use ivy-read

--- config/rk-org/rk-org-jira-url.el
+++ config/rk-org/rk-org-jira-url.el
@@ -8,18 +8,30 @@
 
 ;;; Code:
 
-(autoload 's-upcase "s")
 (autoload 'org-insert-link "org")
+(autoload 'ivy-read "ivy")
 
 (defconst rk-org--jira-base-url-template
-  "https://moviohq.atlassian.net/browse/%s")
+  "https://moviohq.atlassian.net/browse/%s"
+  "Base URL for work JIRA.")
+
+(defconst rk-org--jira-project-alist
+  '(("Green" . "GREEN")
+    ("Support" . "MCSUP")
+    ("Blue" . "BLUE")
+    ("Yellow" . "YE")
+    ("Cinema" . "MC"))
+  "Map of project names to abbreviated tags.")
 
 (defun rk-org--create-jira-url (ticket project)
   "Create a URL for TICKET, scoped to the Green project by default, unless PROJECT is specified."
   (interactive (let ((ticket (read-string "Ticket number: " nil))
-                     (project (read-string "Project: " "Green")))
+                     (project (ivy-read "Project: " '("Green" "Support" "Red" "Blue" "Yellow" "Cinema")
+                                        :predicate nil
+                                        :require-match t)))
                  (list ticket project)))
-  (let ((ticket-and-project (format "%s-%s" (s-upcase project) ticket)))
+  (let* ((project-tag (cdr (assoc project rk-org--jira-project-alist)))
+         (ticket-and-project (format "%s-%s" project-tag ticket)))
     (org-insert-link nil (format rk-org--jira-base-url-template ticket-and-project) nil)))
 
 (provide 'rk-org-jira-url)
