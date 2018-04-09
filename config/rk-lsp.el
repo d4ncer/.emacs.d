55e8efcd3340395b6354825ee3b8d2f1353f9845
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2018-04-10 07:51:16 +1200

    Use purples in lsp-ui-imenu

--- config/rk-lsp.el
+++ config/rk-lsp.el
@@ -20,9 +20,12 @@
     (add-hook 'lsp-mode-hook #'lsp-ui-mode)))
 
 (use-package lsp-imenu
+  :defines (lsp-ui-imenu-colors)
   :commands (lsp-enable-imenu)
   :config
-  (add-hook 'lsp-after-open-hook #'lsp-enable-imenu))
+  (progn
+    (setq lsp-ui-imenu-colors '("#e99ce8" "#bbbbff" "#ffbbff"))
+    (add-hook 'lsp-after-open-hook #'lsp-enable-imenu)))
 
 (provide 'rk-lsp)
 
