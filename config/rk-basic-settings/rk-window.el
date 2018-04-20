eb4f76c2c76f7dc7213a6dfb95e69b265e9d1b7e
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2018-04-20 17:14:17 +1200

    Add empty file (to fill)

---
+++ config/rk-basic-settings/rk-window.el
@@ -0,0 +1,13 @@
+;;; rk-window.el --- Window hydra  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017 Raghuvir Kasturi
+
+;; Author: Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
+
+;;; Commentary:
+
+;;; Code:
+
+(provide 'rk-window)
+
+;;; rk-window.el ends here
