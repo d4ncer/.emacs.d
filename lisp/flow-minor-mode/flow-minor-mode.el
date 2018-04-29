adade8b8d64e2d2b4bebfba1ba5e4495af839a0a
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2018-04-30 08:14:03 +1200

    Add yet another error to chop off in flow

--- lisp/flow-minor-mode/flow-minor-mode.el
+++ lisp/flow-minor-mode/flow-minor-mode.el
@@ -36,7 +36,8 @@
 (defconst flow-minor-buffer "*Flow Output*")
 
 (defconst flow-minor-garbage-error "Please wait. Server is garbage collecting shared memory: -")
-(defconst flow-minor-handle-error "Please wait. Server is handling a request (starting up): \\")
+(defconst flow-minor-handle-error-1 "Please wait. Server is handling a request (starting up): \\")
+(defconst flow-minor-handle-error-2 "Please wait. Server is handling a request (starting up): -")
 
 (defcustom flow-minor-default-binary "flow"
   "Flow executable to use when no project-specific binary is found."
@@ -168,7 +169,11 @@ BODY progn"
           (line (number-to-string (line-number-at-pos)))
           (col (number-to-string (1+ (current-column))))
           (type (flow-minor-cmd-to-string "type-at-pos" file line col)))
-     (message "%s" (flow-minor-colorize-type (s-chop-prefixes (list flow-minor-garbage-error flow-minor-handle-error) (car (s-lines type))))))))
+     (message "%s" (flow-minor-colorize-type (s-chop-prefixes
+                                              (list flow-minor-garbage-error
+                                                    flow-minor-handle-error-1
+                                                    flow-minor-handle-error-2)
+                                              (car (s-lines type))))))))
 
 (defun flow-minor-jump-to-definition ()
   "Jump to definition."
