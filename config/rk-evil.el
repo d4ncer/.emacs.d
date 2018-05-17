705f526a3ecf8185e2b90e0b8233ee3bc01e6301
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2018-05-18 11:38:46 +1200

    Tweak bindings for evil-multiedit

--- config/rk-evil.el
+++ config/rk-evil.el
@@ -120,10 +120,12 @@
     (define-key evil-multiedit-state-map (kbd "RET") 'evil-multiedit-toggle-or-restrict-region)
     (define-key evil-motion-state-map (kbd "RET") 'evil-multiedit-toggle-or-restrict-region)
 
-    (define-key evil-multiedit-state-map (kbd "C-n") 'evil-multiedit-next)
-    (define-key evil-multiedit-state-map (kbd "C-p") 'evil-multiedit-prev)
-    (define-key evil-multiedit-insert-state-map (kbd "C-n") 'evil-multiedit-next)
-    (define-key evil-multiedit-insert-state-map (kbd "C-p") 'evil-multiedit-prev)))
+    (define-key evil-multiedit-state-map (kbd "C") nil)
+    (define-key evil-multiedit-state-map (kbd "S") #'evil-multiedit--substitute)
+    (define-key evil-multiedit-state-map (kbd "C-n") #'evil-multiedit-next)
+    (define-key evil-multiedit-state-map (kbd "C-p") #'evil-multiedit-prev)
+    (define-key evil-multiedit-insert-state-map (kbd "C-n") #'evil-multiedit-next)
+    (define-key evil-multiedit-insert-state-map (kbd "C-p") #'evil-multiedit-prev)))
 
 (use-package evil-ex
   :defer t
