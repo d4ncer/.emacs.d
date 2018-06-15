a5f0e1025bd64902b5fdad8a6dc297af18d625a5
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2018-06-15 22:21:09 +1200

    Clean up unused modeline experiments. Also Rihanna.

--- config/rk-modeline.el
+++ config/rk-modeline.el
@@ -13,41 +13,9 @@
 
 (require 'spacemacs-keys)
 
-;;; TODO THIS NEEDS WORK WORK WORK WORK WORK (sung in a Rihanna voice)
-;; (use-package spaceline
-;;   :straight t)
-
-;; (use-package spaceline-config
-;;   :after spaceline
-;;   :config
-;;   (progn
-;;     (spaceline-toggle-minor-modes-off)
-;;     (spaceline-emacs-theme)))
-
-;; (use-package rk-mode-line-format
-;;   :defines rk-mode-line-format
-;;   :config
-;;   (setq-default mode-line-format rk-mode-line-format))
-
 (use-package doom-modeline
   :config
   (+doom-modeline|init))
-
-;; (use-package hidden-mode-line
-;;   :commands (hidden-mode-line-mode global-hidden-mode-line-mode)
-;;   :init
-;;   (setq-default mode-line-format " "))
-
-;; (use-package rk-header-line-mode
-;;   :commands (rk-header-line-global-mode rk-header-line-mode rk-header-line-mode-on)
-;;   :init
-;;   (progn
-;;     (spacemacs-keys-set-leader-keys
-;;       "TM" #'rk-header-line-mode
-;;       "Tm" #'rk-header-line-global-mode)
-;;     (add-hook 'after-init-hook #'rk-header-line-global-mode))
-;;   :config
-;;   (setq rk-header-line-function (lambda () rk-header-line-format)))
 
 (provide 'rk-modeline)
 
