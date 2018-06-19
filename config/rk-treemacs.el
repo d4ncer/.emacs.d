074758a3379fb0d6f983853a3734175e649ce095
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2018-06-19 20:05:34 +1200

    Setup treemacs persist before treemacs loads

--- config/rk-treemacs.el
+++ config/rk-treemacs.el
@@ -32,16 +32,17 @@
           (force-mode-line-update)))))
   :config
   (progn
-    (setq treemacs--persist-file (concat paths-cache-directory "/treemacs-persist"))
     (evil-set-initial-state 'treemacs-mode 'emacs)
     (define-key treemacs-mode-map (kbd "j") #'treemacs-next-line)
     (define-key treemacs-mode-map (kbd "k") #'treemacs-previous-line)
     (define-key treemacs-mode-map (kbd "SPC") spacemacs-keys-default-map)
     (add-hook 'buffer-list-update-hook #'rk-treemacs--setup-header-line))
   :init
-  (spacemacs-keys-set-leader-keys
-    "f t" #'treemacs
-    "p t" #'treemacs-add-project))
+  (progn
+    (setq treemacs--persist-file (concat paths-cache-directory "/treemacs-persist"))
+    (spacemacs-keys-set-leader-keys
+      "f t" #'treemacs
+      "p t" #'treemacs-add-project)))
 
 (provide 'rk-treemacs)
 
