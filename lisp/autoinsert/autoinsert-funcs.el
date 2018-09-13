91b164d57bd22a953de6ba9d7f3197551bc8e9f2
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2018-09-14 08:54:16 +1200

    Clean up auto insert for new el files

--- lisp/autoinsert/autoinsert-funcs.el
+++ lisp/autoinsert/autoinsert-funcs.el
@@ -16,30 +16,12 @@
 
 ;;; Elisp
 
-(defconst autoinsert-funcs-el-gnu-license "
-\;; This program is free software; you can redistribute it and/or modify
-\;; it under the terms of the GNU General Public License as published by
-\;; the Free Software Foundation, either version 3 of the License, or
-\;; (at your option) any later version.
-\;; This program is distributed in the hope that it will be useful,
-\;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-\;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-\;; GNU General Public License for more details.
-\;; You should have received a copy of the GNU General Public License
-\;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-")
-
 (defconst autoinsert-funcs-el-form
   '((emacs-lisp-mode . "Emacs Lisp")
     nil
     "\;;; " (file-name-nondirectory (buffer-file-name)) " --- <enter description here>  "
     "-*- lexical-binding: t; -*-" '(setq lexical-binding t) \n
     \n
-    ";; Copyright (C) " (format-time-string "%Y") "  "
-    (getenv "ORGANIZATION") | user-full-name                \n
-    \n
-    ";; Author: " user-full-name " <" user-mail-address ">" \n
-    autoinsert-funcs-el-gnu-license                            \n
     ";;; Commentary:"                                       \n \n
     ";;; Code:"                                             \n \n
     _                                                       \n \n
