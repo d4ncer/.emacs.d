936490db0a859431fc5c7f0fb710ff49a8615241
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2018-12-25 21:23:37 +1300

    [rust] Fix keymap

--- config/rk-rust.el
+++ config/rk-rust.el
@@ -45,7 +45,7 @@
     ;; Enable backtraces in rust programs run from Emacs.
     (setenv "RUST_BACKTRACE" "1")
 
-    (general-def :keymaps 'rust-mode-map :states '(insert normal emacs motion visual)
+    (general-def :keymaps 'rust-mode-map :states '(insert)
       "TAB" #'company-indent-or-complete-common)
     (general-def :keymaps 'rust-mode-map :states 'normal
       "J" #'rk-rust--join-line)
