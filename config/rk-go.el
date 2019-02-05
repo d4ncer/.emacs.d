4757d74df0d886f2100af7184965645ce95aeeed
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2019-02-06 11:11:08 +1300

    [go] Favour LSP over gocode

--- config/rk-go.el
+++ config/rk-go.el
@@ -20,88 +20,10 @@
 (use-package go-mode
   :straight t
   :mode ("\\.go\\'" . go-mode)
-
-  :general
-  (:keymaps 'go-mode-map :states 'normal
-            "gd" #'godef-jump
-            "K" #'godoc-at-point)
-  (:keymaps 'go-mode-map :states 'insert
-            "M-." #'godef-jump)
-
-  :preface
-  (progn
-    (defun rk-go-lookup-go-root ()
-      (-let* ((default-directory (or (projectile-project-p) default-directory))
-              (output (s-lines (s-trim (shell-command-to-string "go env"))))
-              ((&alist "GOROOT" go-root)
-               (--map (-let* (((var val) (s-split "=" it))
-                             ((_ val) (s-match (rx "\"" (group (*? nonl)) "\"") val)))
-                       (cons var val))
-                     output)))
-        go-root))
-
-    (defun rk-go--set-local-vars ()
-      (setq-local tab-width 4)
-      (setq-local indent-tabs-mode t)
-      (setq-local compile-command "go build -v")
-      (with-no-warnings
-        (setq-local evil-shift-width 4))
-      (unless (getenv "GOROOT")
-        (setenv "GOROOT" (rk-go-lookup-go-root)))))
-
+  :after lsp-mode
   :config
-  (progn
-    (setq gofmt-command "goreturns")
-    (setq godoc-at-point-function 'godoc-gogetdoc)
-    (setq gofmt-show-errors nil)
-    (rk-local-leader-def :keymaps 'go-mode-map
-      "r" '(:ignore t :wk "refactor")
-
-      "g" '(:ignore t :wk "goto")
-      "g g" '(godef-jump :wk "jump to def")
-      "g d" '(godef-describe :wk "describe")
-      "g w" '(godef-jump-other-window :wk "jump to def (other window)")
-      "g c" '(go-coverage :wk "coverage")
-
-      "i" '(:ignore t :wk "imports")
-      "i g" '(go-goto-imports :wk "go to imports")
-      "i a" '(go-import-add :wk "add import")
-      "i r" '(go-remove-unused-imports :wk "remove unused"))
-
-    (add-hook 'go-mode-hook #'rk-go--set-local-vars)
-    (add-hook 'before-save-hook #'gofmt-before-save))
-
-  :functions (gofmt-before-save godoc-at-point))
-
-(use-package go-rename
-  :straight t
-  :after go-mode
-  :config
-  (rk-local-leader-def :keymaps 'go-mode-map
-    "r r" '(go-rename :wk "rename")))
-
-(use-package company-go
-  :straight t
-  :after go-mode
-
-  :preface
-  (progn
-    (autoload 'company-mode "company")
-
-    (defun rk-go-company-setup ()
-      (set (make-local-variable 'company-backends) '(company-go))
-      (company-mode)))
-
-  :config
-  (progn
-    (with-no-warnings
-      (setq company-go-show-annotation t))
-    (add-hook 'go-mode-hook #'rk-go-company-setup)))
-
-(use-package go-eldoc
-  :straight t
-  :after go-mode
-  :config (add-hook 'go-mode-hook 'go-eldoc-setup))
+  (add-hook 'go-mode-hook #'lsp)
+  (add-hook 'lsp-go-mode-hook #'flycheck-mode))
 
 (use-package go-tag
   :straight t
