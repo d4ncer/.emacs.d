f2cc751abcff887681addd66e25e341e5237a529
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2019-02-06 18:16:02 +1300

    [lsp] Make LSP buffers closeable via C-g

--- config/rk-basic-settings.el
+++ config/rk-basic-settings.el
@@ -629,7 +629,7 @@ Optional arg JUSTIFY will justify comments and strings."
     ;; (let* ((trp (rx "*" (or "Cargo" "go" "Racer" "restclient") (zero-or-more anything) "*"))
     ;;        (trp-1? (if (s-matches-p trp "*go *") "YES" "NO")))
     ;;   (message trp-1?))
-    (push (list (rx "*" (or "Cargo" "go" "Racer" "restclient" "Ledger") (zero-or-more anything) "*") :noselect t :regexp t) popwin:special-display-config)
+    (push (list (rx "*" (or "Cargo" "go" "Racer" "restclient" "Ledger" "lsp") (zero-or-more anything) "*") :noselect t :regexp t) popwin:special-display-config)
     (popwin-mode 1)))
 
 (use-package keychain-environment
