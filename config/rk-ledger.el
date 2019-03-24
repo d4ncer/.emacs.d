6cbfe4b6734d6115ca5c97792eb896929be7b882
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2019-03-25 07:56:21 +1300

    [ledger] Add Chris' formatting utils

--- config/rk-ledger.el
+++ config/rk-ledger.el
@@ -7,9 +7,15 @@
 (eval-when-compile
   (require 'use-package))
 
+(require 'definers)
 (use-package ledger-mode
   :mode ("\\.ledger$" . ledger-mode)
   :straight t)
+(use-package cb-ledger-format
+  :after ledger-mode
+  :init
+  (rk-local-leader-def :keymaps 'ledger-mode-map
+    "." '(cb-ledger-format-buffer :wk "format")))
 
 (use-package flycheck-ledger
   :straight t
