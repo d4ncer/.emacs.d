02163ce6c3ebb6ba159bccf15f0a2b93b04db8c6
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2019-06-20 06:33:14 +0530

    [rg] Add deadgrip frontend
    Thanks @chrisbarrett!

--- config/rk-search.el
+++ config/rk-search.el
@@ -35,6 +35,38 @@
   (progn
     (setq wgrep-auto-save-buffer t)))
 
+(use-package deadgrep
+  :straight t
+  :commands (deadgrep)
+  :general (:keymaps 'deadgrep-mode-map "C-c C-w" #'deadgrep-edit-mode)
+
+  :preface
+  (defun rk-search--deadgrep-requery ()
+    (interactive)
+    (let ((button (save-excursion
+                    (goto-char (point-min))
+                    (forward-button 1))))
+      (button-activate button)))
+  :general (:states 'normal :keymaps 'deadgrep-mode-map
+                    "c" #'rk-search--deadgrep-requery)
+
+  :preface
+  (defun rk-search--on-enter-deadgrep-edit-mode (&rest _)
+    (message "Entering edit mode. Changes will be made to underlying files as you edit."))
+  :config
+  (advice-add #'deadgrep-edit-mode :after #'rk-search--on-enter-deadgrep-edit-mode)
+
+  :preface
+  (defun rk-search--on-exit-deadgrep-edit-mode (&rest _)
+    (when (derived-mode-p 'deadgrep-edit-mode)
+      (message "Exiting edit mode.")))
+
+  :init
+  (rk-leader-def
+    "sd" '(deadgrep :wk "deadgrep"))
+  :config
+  (advice-add #'deadgrep-mode :before #'rk-search--on-exit-deadgrep-edit-mode))
+
 (provide 'rk-search)
 
 ;;; rk-search.el ends here
