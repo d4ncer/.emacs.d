9be0a07b75ada13b82799faa1a3f209beb6bad09
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2019-10-11 13:53:34 +1300

    [vterm] More config and cleanup

--- config/rk-vterm.el
+++ config/rk-vterm.el
@@ -7,6 +7,8 @@
 (eval-when-compile
   (require 'use-package))
 
+(require 'general)
+
 (use-package vterm
   :straight t
   :general
@@ -14,18 +16,22 @@
   (:keymaps 'vterm-mode-map :states '(normal insert) "s-v" 'vterm-yank)
   :preface
   (progn
-    (defun config-terminal--build-vterm (package &rest _)
+    (defun rk-vterm--setup ()
+      (flycheck-mode -1))
+    (defun rk-vterm--build-vterm (package &rest _)
       (when (member package '("vterm"))
         (let* ((root (straight--build-dir "vterm"))
                (module-build-dir (f-join root "build")))
           (mkdir module-build-dir t)
           (let ((default-directory module-build-dir))
             (compilation-start "cmake .. && make")))))
-    (add-hook 'straight-use-package-pre-build-functions #'config-terminal--build-vterm))
+    (add-hook 'straight-use-package-pre-build-functions #'rk-vterm--build-vterm))
   :config
-  ;; Prevent vterm from handling function keys.
-  (dotimes (n 12)
-    (define-key vterm-mode-map (kbd (format "<f%s>" (1+ n))) nil)))
+  (progn
+    (add-hook 'vterm-mode-hook #'rk-vterm--setup)
+    ;; Prevent vterm from handling function keys.
+    (dotimes (n 12)
+      (define-key vterm-mode-map (kbd (format "<f%s>" (1+ n))) nil))))
 
 (use-package vterm-toggle
   :straight (:host github :repo "jixiuf/vterm-toggle")
