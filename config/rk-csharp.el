02869f77a493737e7a077e6de758f3a94e4ef303
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2020-05-13 10:04:13 +1200

    [csharp] Fix comment to be lispy

--- config/rk-csharp.el
+++ config/rk-csharp.el
@@ -14,7 +14,7 @@
   :straight t
   ;; :custom
   ;; (lsp-csharp-server-path (expand-file-name "~/code/omnisharp-roslyn/artifacts/scripts/OmniSharp.Stdio"))
-  ;; :hook (csharp-mode . lsp)
+  ;; :hook (csharp-mode . lsp))
   :preface
   (defun rk-csharp--setup ()
     (company-mode)
