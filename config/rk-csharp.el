9cbf89022aa34089e166ddc0d2bd06dffa9b9f87
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2020-06-15 14:09:09 +1200

    [c#] Go back to LSP

--- config/rk-csharp.el
+++ config/rk-csharp.el
@@ -9,44 +9,18 @@
 
 (require 'lsp)
 
-;;; TODO Come back to this once I can build razzmatazz's branch
 (use-package csharp-mode
   :straight t
-  ;; :custom
-  ;; (lsp-csharp-server-path (expand-file-name "~/code/omnisharp-roslyn/artifacts/scripts/OmniSharp.Stdio"))
-  ;; :hook (csharp-mode . lsp))
-  :preface
-  (defun rk-csharp--setup ()
-    (company-mode)
-    (eval-after-load
-        'company
-      '(add-to-list 'company-backends 'company-omnisharp))
-    (flycheck-mode)
-
-    (setq indent-tabs-mode nil)
-    (setq c-syntactic-indentation t)
-    (c-set-style "ellemtel")
-    (setq c-basic-offset 4)
-    (setq truncate-lines t)
-    (setq tab-width 4)
-    (setq evil-shift-width 4)
-
-    (local-set-key (kbd "C-c r r") 'omnisharp-run-code-action-refactoring)
-    (local-set-key (kbd "C-c C-c") 'recompile))
-  :hook
-  (csharp-mode . rk-csharp--setup))
-
-(use-package omnisharp
-  :straight t
-  :after csharp-mode
-  :hook
-  (csharp-mode . omnisharp-mode)
-  :general
-  (:keymaps 'omnisharp-mode-map :states '(normal motion visual)
-            "gd" #'omnisharp-go-to-definition))
+  :custom
+  (lsp-csharp-server-path "/usr/local/bin/omnisharp")
+  :hook (csharp-mode . lsp))
 
 (use-package csproj-mode
   :straight t)
+
+(use-package powershell
+  :straight t
+  :hook (powershell-mode . lsp))
 
 (provide 'rk-csharp)
 
