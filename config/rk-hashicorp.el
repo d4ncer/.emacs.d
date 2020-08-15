c9e9e1a5ae7b4f8d6e700c42b8240857b3273d0a
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2020-08-16 10:22:28 +1200

    [terraform] Set up official LSP

--- config/rk-hashicorp.el
+++ config/rk-hashicorp.el
@@ -7,6 +7,8 @@
 (eval-when-compile
   (require 'use-package))
 
+(require 'lsp)
+
 (use-package hcl-mode
   :straight t
   :mode (("\\.hcl\\'" . hcl-mode)
@@ -15,8 +17,23 @@
 (use-package terraform-mode
   :straight t
   :mode ("\\.tf\\(vars\\)?\\'" . terraform-mode)
+  :preface
+  (defun rk-hashicorp--terra-ls-server-cmd ()
+    (let* ((base '("terraform-ls" "serve" "-tf-exec"))
+           (tbin (executable-find "terraform")))
+      (-snoc base tbin)))
+  (defun rk-hashicorp--register-official-lsp-client ()
+    (lsp-register-client
+     (make-lsp-client :new-connection (lsp-stdio-connection (lambda () (rk-hashicorp--terra-ls-server-cmd)))
+                      :major-modes '(terraform-mode)
+                      :priority 1
+                      :server-id 'terrals)))
+  (defun rk-hashicorp--setup-lsp ()
+    (rk-hashicorp--register-official-lsp-client)
+    (lsp-deferred))
   :config
-  (add-hook #'terraform-mode-hook #'terraform-format-on-save-mode))
+  (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode)
+  (add-hook 'terraform-mode-hook #'rk-hashicorp--setup-lsp))
 
 (provide 'rk-hashicorp)
 
