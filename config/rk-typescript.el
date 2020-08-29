202c1158d1ade2584595d28aca4f839aa1d418bf
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2020-08-29 20:58:00 +1200

    [typescript] Enhance LSP & DX
    - Add node modules to path for better scoped binaries
    - Use TS + ESLint lang servers in tandem
    - Teach flycheck to use both language servers

--- config/rk-typescript.el
+++ config/rk-typescript.el
@@ -8,14 +8,23 @@
   (require 'use-package))
 
 (require 'lsp)
+(require 'add-node-modules-path)
+(autoload 'lsp-diagnostics-flycheck-enable "lsp-diagnostics")
 
 (use-package typescript-mode
   :straight t
   :mode ("\\.tsx?\\'" . typescript-mode)
+  :preface
+  (defun rk-ts--add-eslint (&rest _)
+    (flycheck-add-next-checker 'lsp 'javascript-eslint))
+  (defun rk-ts--setup-typescript ()
+    (add-node-modules-path)
+    (lsp))
   :custom
   (typescript-indent-level 2)
-  :config
-  (add-hook 'typescript-mode-hook #'lsp))
+  :init
+  (advice-add 'lsp-diagnostics-flycheck-enable :after #'rk-ts--add-eslint)
+  (add-hook 'typescript-mode-hook #'rk-ts--setup-typescript))
 
 (provide 'rk-typescript)
 
