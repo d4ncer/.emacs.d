e87fc1caaa4722b99adb4b948dbd3b7003ab000e
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2020-09-04 07:09:04 +1200

    [plantuml] Go back to exec & let the PATH choose

--- config/rk-basic-settings.el
+++ config/rk-basic-settings.el
@@ -771,8 +771,8 @@ Optional arg JUSTIFY will justify comments and strings."
 (use-package plantuml-mode
   :straight t
   :custom
-  (plantuml-default-exec-mode 'jar)
-  (plantuml-jar-path "/Users/raghuvirkasturi/.local/jars/plantuml.jar"))
+  (plantuml-default-exec-mode 'executable)
+  (plantuml-executable-path "plantuml"))
 
 (provide 'rk-basic-settings)
 
