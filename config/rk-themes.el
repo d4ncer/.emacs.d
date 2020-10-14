827db40ccb67ac0c2431810b60ed9cf60bc5e274
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2020-10-15 10:51:21 +1300

    [base] Ligatures! For real!

--- config/rk-themes.el
+++ config/rk-themes.el
@@ -16,39 +16,13 @@
 (require 'dash)
 (require 'general)
 
-;; (use-package rk-ligatures
-;;   :if (window-system)
-;;   :preface
-;;   (autoload 'rk-ligatures--set-kwds "rk-ligatures")
-;;   :config
-;;   (rk-ligatures--set-kwds
-;;    '((rk-ligatures--fira-font prog-mode-hook))))
-
-;; (use-package rk-iosevka
-;;   :if (window-system))
+(use-package rk-ligature
+  :if (window-system))
 
 (defun rk/themes/light-theme ()
   "Load light theme."
   (interactive)
   (load-theme 'rk-light t))
-
-(use-package doom-themes
-  :straight t
-  :config
-  (progn
-    (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-          doom-themes-enable-italic t) ; if nil, italics is universally disabled
-    ;; Enable flashing mode-line on errors
-    (doom-themes-visual-bell-config)
-
-    ;; Enable custom neotree theme
-    (doom-themes-neotree-config)  ; all-the-icons fonts must be installed!
-
-    ;; Corrects (and improves) org-mode's native fontification.
-    (doom-themes-org-config)))
-
-(use-package tao-theme
-  :straight t)
 
 (defun config-themes--after-enable-theme (&rest _)
   ;; Delete posframes after changing themes.
---
+++ lisp/rk-themes/rk-ligature.el
@@ -0,0 +1,28 @@
+;;; rk-ligature.el --- Config for ligatures  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(eval-when-compile
+  (require 'use-package))
+
+(use-package ligature
+  :straight (:host github :repo "mickeynp/ligature.el" :branch "master")
+  :config
+  (ligature-set-ligatures 't '("www"))
+  (ligature-set-ligatures 'prog-mode '("www" "**" "***" "**/" "*>" "*/" "\\\\" "\\\\\\" "{-" "::"
+                                       ":::" ":=" "!!" "!=" "!==" "-}" "----" "-->" "->" "->>"
+                                       "-<" "-<<" "-~" "#{" "#[" "##" "###" "####" "#(" "#?" "#_"
+                                       "#_(" ".-" ".=" ".." "..<" "..." "?=" "??" ";;" "/*" "/**"
+                                       "/=" "/==" "/>" "//" "///" "&&" "||" "||=" "|=" "|>" "^=" "$>"
+                                       "++" "+++" "+>" "=:=" "==" "===" "==>" "=>" "=>>" "<="
+                                       "=<<" "=/=" ">-" ">=" ">=>" ">>" ">>-" ">>=" ">>>" "<*"
+                                       "<*>" "<|" "<|>" "<$" "<$>" "<!--" "<-" "<--" "<->" "<+"
+                                       "<+>" "<=" "<==" "<=>" "<=<" "<>" "<<" "<<-" "<<=" "<<<"
+                                       "<~" "<~~" "</" "</>" "~@" "~-" "~>" "~~" "~~>" "%%"))
+  (global-ligature-mode t))
+
+(provide 'rk-ligature)
+
+;;; rk-ligature.el ends here
