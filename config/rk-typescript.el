0f5c537481500aca370579f31f688df73984c04a
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2021-09-23 11:04:01 +1200

    [ts] Setup emmet mode in TS

--- config/rk-typescript.el
+++ config/rk-typescript.el
@@ -98,6 +98,12 @@
   :init
   (add-hook 'typescript-mode #'add-node-modules-path))
 
+(use-package emmet-mode
+  :straight t
+  :after typescript-mode
+  :init
+  (add-hook 'typescript-mode-hook #'emmet-mode))
+
 (provide 'rk-typescript)
 
 ;;; rk-typescript.el ends here
