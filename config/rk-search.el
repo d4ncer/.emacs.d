93a66f53931fc046d6d10483def4bf177ab4bb4d
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2021-12-07 09:07:58 +1300

    [search] Clean up
    - Remove rg/ag
    - Fix wgrep-setup hook (how did this ever work)
    - Clean up deadgrep config

--- config/rk-search.el
+++ config/rk-search.el
@@ -13,32 +13,22 @@
 
 (require 'rk-misc-utils)
 
-(use-package ag
-  :straight t
-  :commands (ag ag-dired))
-
-(use-package rg
-  :straight t
-  :commands rg)
-
 (use-package wgrep
   :straight t
-  :commands (wgrep-setup)
   :init
-  (add-hook 'grep-setup-hook #'wgrep-stup)
+  (add-hook 'grep-setup-hook #'wgrep-setup)
+  :custom
+  (wgrep-auto-save-buffer t)
   :general
   (:keymaps 'wgrep-mode-map :states '(normal motion visual emacs)
             ", c" #'rk-search-wgrep-finish-edit-kill-buffer
-            ", k" #'rk-search-wgrep-abort-changes-kill-buffer)
-  :config
-  (progn
-    (setq wgrep-auto-save-buffer t)))
+            ", k" #'rk-search-wgrep-abort-changes-kill-buffer))
 
 (use-package deadgrep
   :straight t
-  :commands (deadgrep)
   :general (:keymaps 'deadgrep-mode-map "C-c C-w" #'deadgrep-edit-mode)
-
+  (:states 'normal :keymaps 'deadgrep-mode-map
+           "c" #'rk-search--deadgrep-requery)
   :preface
   (defun rk-search--deadgrep-requery ()
     (interactive)
@@ -46,24 +36,16 @@
                     (goto-char (point-min))
                     (forward-button 1))))
       (button-activate button)))
-  :general (:states 'normal :keymaps 'deadgrep-mode-map
-                    "c" #'rk-search--deadgrep-requery)
-
-  :preface
   (defun rk-search--on-enter-deadgrep-edit-mode (&rest _)
     (message "Entering edit mode. Changes will be made to underlying files as you edit."))
-  :config
-  (advice-add #'deadgrep-edit-mode :after #'rk-search--on-enter-deadgrep-edit-mode)
-
-  :preface
   (defun rk-search--on-exit-deadgrep-edit-mode (&rest _)
     (when (derived-mode-p 'deadgrep-edit-mode)
       (message "Exiting edit mode.")))
-
   :init
   (rk-leader-def
     "sd" '(deadgrep :wk "deadgrep"))
   :config
+  (advice-add #'deadgrep-edit-mode :after #'rk-search--on-enter-deadgrep-edit-mode)
   (advice-add #'deadgrep-mode :before #'rk-search--on-exit-deadgrep-edit-mode))
 
 (provide 'rk-search)
