3911ab1d5d8a00c4c92c45af11f1d20b4dcc6fd8
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2021-12-07 19:15:14 +1300

    [magit] Clean up
    - Replace evil transient state with pretty-hydra
    - Clean up config

--- config/rk-magit.el
+++ config/rk-magit.el
@@ -11,9 +11,7 @@
 (eval-when-compile
   (require 'use-package))
 
-(require 'evil-transient-state)
 (require 'definers)
-(require 'general)
 (require 'pretty-hydra)
 
 (use-package with-editor
@@ -28,16 +26,18 @@
 
 (use-package transient
   :straight t
-  :init
-  (setq transient-values-file (f-join paths-cache-directory "transient/values.el"))
-  (setq transient-history-file (f-join paths-cache-directory "transient/history.el"))
-  (setq transient-levels-file (f-join paths-cache-directory "transient/levels.el")))
+  :custom
+  (transient-values-file (f-join paths-cache-directory "transient/values.el"))
+  (transient-history-file (f-join paths-cache-directory "transient/history.el"))
+  (transient-levels-file (f-join paths-cache-directory "transient/levels.el")))
 
 (use-package magit
   :straight t
-  :defer t
-  :commands (magit-status magit-blame-addition magit-branch-and-checkout)
   :functions (magit-display-buffer-fullframe-status-v1)
+  :custom
+  (magit-log-section-commit-count 0)
+  (magit-section-visibility-indicator nil)
+  (magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
   :general
   (:keymaps 'magit-refs-mode-map :states '(normal)
             "." #'magit-branch-and-checkout)
@@ -51,15 +51,10 @@
   :init
   (rk-leader-def
     "gs" #'magit-status
-    "gb" #'rk-git--git-blame/body)
-  :config
-  (general-setq magit-log-section-commit-count 0
-                magit-section-visibility-indicator nil
-                magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
+    "gb" #'rk-git--git-blame/body))
 
 (use-package git-commit-clubhouse-prefix
   :disabled t
-  :commands git-commit-clubhouse-prefix-init
   :config (git-commit-clubhouse-prefix-init))
 
 (use-package git-commit-jira-prefix
@@ -93,41 +88,44 @@
 
 (use-package diff-hl
   :straight t
-  :after magit
   :preface
-  (progn
-    (defun rk-magit--diff-hl-mode-on ()
-      (diff-hl-mode -1))
-
-    (defun rk-magit--diff-hl-mode-off ()
-      (diff-hl-mode +1)))
+  (pretty-hydra-define rk-git--git-hunks
+    (:title "Git Hunks" :foreign-keys run :quit-key "q")
+    ("Navigation"
+     (("n" diff-hl-next-hunk "Next")
+      ("N" diff-hl-previous-hunk "Previous")
+      ("p" diff-hl-previous-hunk "Previous"))
+     "Actions"
+     (("g" diff-hl-diff-goto-hunk "Goto")
+      ("x" diff-hl-revert-hunk "Revert"))))
   :init
-  (progn
-    (evil-transient-state-define git-hunks
-      :title "Git Hunk Transient State"
-      :doc "
-[_p_/_N_] previous [_n_] next [_g_] goto [_x_] revert [_q_] quit"
-      :foreign-keys run
-      :bindings
-      ("n" diff-hl-next-hunk)
-      ("N" diff-hl-previous-hunk)
-      ("p" diff-hl-previous-hunk)
-      ("g" diff-hl-diff-goto-hunk)
-      ("x" diff-hl-revert-hunk)
-      ("q" nil :exit t))
-
-    (rk-leader-def
-      "g." 'git-hunks-transient-state/body))
+  (rk-leader-def
+    "g." 'rk-git--git-hunks/body)
   :config
-  (progn
-    (add-hook 'iedit-mode-hook #'rk-magit--diff-hl-mode-on)
-    (add-hook 'iedit-mode-end-hook #'rk-magit--diff-hl-mode-off)
-    (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
-    (global-diff-hl-mode)))
+  (global-diff-hl-mode))
+
+(use-package diff-hl
+  :straight t
+  :after iedit
+  :preface
+  (defun rk-magit--diff-hl-mode-on ()
+    (diff-hl-mode -1))
+
+  (defun rk-magit--diff-hl-mode-off ()
+    (diff-hl-mode +1))
+  :config
+  (add-hook 'iedit-mode-hook #'rk-magit--diff-hl-mode-on)
+  (add-hook 'iedit-mode-end-hook #'rk-magit--diff-hl-mode-off))
+
+(use-package diff-hl
+  :straight t
+  :after magit
+  :config
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
 
 (use-package forge
-  :after magit
   :straight (:host github :repo "magit/forge")
+  :after magit
   :custom
   (forge-add-default-sections nil)
   (forge-add-default-bindings nil))
