16b94acfc39503ef4d2e0289ef8727b44e105767
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2021-12-08 09:09:50 +1300

    [flycheck] Re-implement hydra via pretty-hydra

--- lisp/rk-flycheck/flycheck-transient-state.el
+++ lisp/rk-flycheck/flycheck-transient-state.el
@@ -10,23 +10,20 @@
 
 ;;; Code:
 
-(require 'evil-transient-state)
+(require 'pretty-hydra)
 (require 'flycheck)
 
-(evil-transient-state-define rk-flycheck-ts
-  :on-enter (progn (setq-local hydra-hint-display-type 'lv) (flycheck-list-errors))
-  :on-exit (progn (setq-local hydra-hint-display-type 'lv) (quit-windows-on "*Flycheck errors*"))
-  :title "Flycheck errors"
-  :foreign-keys run
-  :doc "
-[_j_] Next [_k_] Previous [_gg_] First [_G_] Last [_f_] Filter [_q_] Quit"
-  :bindings
-  ("f" flycheck-error-list-set-filter)
-  ("j" flycheck-next-error)
-  ("k" flycheck-previous-error)
-  ("gg" flycheck-first-error)
-  ("G" (progn (goto-char (point-max)) (flycheck-previous-error)))
-  ("q" nil))
+(pretty-hydra-define rk-flycheck
+  (:title "Flycheck"
+          :quit-key "q"
+          :foreign-keys run)
+  ("Navigation"
+   (("j" flycheck-next-error "next")
+    ("k" flycheck-previous-error "previous")
+    ("gg" flycheck-first-error "first")
+    ("G" (progn (goto-char (point-max)) (flycheck-previous-error)) "last"))
+   "Action"
+   (("f" flycheck-error-list-set-filter "filter"))))
 
 (provide 'flycheck-transient-state)
 
