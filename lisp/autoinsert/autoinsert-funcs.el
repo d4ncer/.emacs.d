23c847bfe97d72f05064dbd9f235fbbfb219a322
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2021-12-12 16:41:04 +1300

    [auto-insert] Clean up fns

--- lisp/autoinsert/autoinsert-funcs.el
+++ lisp/autoinsert/autoinsert-funcs.el
@@ -9,9 +9,7 @@
 ;;; Code:
 
 (require 'f)
-(require 'subr-x)
 
-(autoload 'projectile-project-p "projectile")
 (autoload 'yas-expand-snippet "yasnippet")
 
 ;;; Elisp
@@ -31,6 +29,7 @@
 ;;; Shell-scripts
 
 (defun autoinsert-funcs--sh-template-string ()
+  "Autoinsert string used for sh buffers"
   (let ((program
          (if (f-ext? (buffer-name) "zsh")
              "zsh"
@@ -49,49 +48,9 @@ $0
     nil
     "# -*- coding: utf-8 -*-\n\n"))
 
-;;; Haskell
-
-(defun autoinsert-funcs-hs-module-name ()
-  (-if-let (root (and (buffer-file-name) (projectile-project-p)))
-      (thread-last (buffer-file-name)
-        (f-no-ext)
-        (s-chop-prefix root)
-        (s-chop-prefixes '("app/" "src/" "test/"))
-        (f-split)
-        (--map (let ((x (substring it 0 1))
-                    (xs (substring it 1)))
-                (concat (s-upcase x) xs)))
-        (s-join "."))
-
-    (s-upper-camel-case (file-name-base))))
-
-(defconst autoinsert-funcs-hs-src-form
-  '((haskell-mode . "Haskell Src File")
-    nil
-    "module " (autoinsert-funcs-hs-module-name) " where" "\n"
-    "\n"
-    _
-    "\n"))
-
-(defconst autoinsert-funcs-hs-test-form
-  '(("Spec\\.hs\\'" . "Haskell Test Spec")
-    nil
-    "module " (autoinsert-funcs-hs-module-name) " where" "\n"
-    "\n"
-    "import           " (s-chop-suffix "Spec" (autoinsert-funcs-hs-module-name)) "\n"
-    "import           Test.Hspec" "\n"
-    "\n"
-    "main :: IO ()" "\n"
-    "main = hspec spec" "\n"
-    "\n"
-    "spec :: Spec" "\n"
-    "spec = do" "\n"
-    "    describe " "\"" _ "\""
-    "\n"))
-
 ;;; HTML
 
-(defun autoinsert-funcs-html-template-string ()
+(defun autoinsert-funcs--html-template-string ()
   (yas-expand-snippet (string-trim
                        "
 <!DOCTYPE html>
@@ -112,8 +71,6 @@ $0
   (list autoinsert-funcs-sh-form
         autoinsert-funcs-el-form
         autoinsert-funcs-py-form
-        autoinsert-funcs-hs-src-form
-        autoinsert-funcs-hs-test-form
         autoinsert-funcs-html-form))
 
 (provide 'autoinsert-funcs)
