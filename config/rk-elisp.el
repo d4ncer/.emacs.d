51ccd045676b1882a48f71a8f2350acc7b105a59
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2021-12-27 12:35:12 +1300

    [elisp] Clean up config

--- config/rk-elisp.el
+++ config/rk-elisp.el
@@ -11,12 +11,7 @@
 (eval-when-compile
   (require 'use-package))
 
-(require 'straight)
-(require 'general)
 (require 'definers)
-(require 'rk-misc-utils)
-(require 'evil)
-(require 'subr-x)
 
 ;; Print a message on `eval-buffer'.
 
@@ -25,43 +20,33 @@
   (defun rk-elisp--message-on-eval-buffer (&rest _)
     (when (called-interactively-p nil)
       (message "Buffer evaluated.")))
+  :init
+  (rk-local-leader-def :keymaps 'emacs-lisp-mode-map
+    "e" '(nil :ignore t :wk "eval")
+    "eb" '(rk-elisp-eval-buffer :wk "eval buffer")
+    "ee" '(eval-expression :wk "eval expression")
+    "es" '(eval-last-sexp :wk "eval last sexp"))
   :config
-  (progn
-    (rk-local-leader-def :keymaps 'emacs-lisp-mode-map
-      "e" '(nil :ignore t :wk "eval")
-      "eb" '(rk-elisp-eval-buffer :wk "eval buffer")
-      "ee" '(eval-expression :wk "eval expression")
-      "es" '(eval-last-sexp :wk "eval last sexp"))
-    (advice-add #'eval-buffer :after #'rk-elisp--message-on-eval-buffer)))
+  (advice-add #'eval-buffer :after #'rk-elisp--message-on-eval-buffer))
 
 
 (use-package elisp-slime-nav
   :straight t
-  :commands (turn-on-elisp-slime-nav-mode
-             elisp-slime-nav-find-elisp-thing-at-point
-             elisp-slime-nav-describe-elisp-thing-at-point)
   :general
-  (:keymaps 'emacs-lisp-mode-map
-            "M-." #'elisp-slime-nav-find-elisp-thing-at-point)
-  :init
-  (progn
-    (general-def 'normal emacs-lisp-mode-map
-      "M-." #'elisp-slime-nav-find-elisp-thing-at-point
-      "K" #'elisp-slime-nav-describe-elisp-thing-at-point)
-    (add-hook 'emacs-lisp-mode-hook #'turn-on-elisp-slime-nav-mode)))
+  (:keymaps 'emacs-lisp-mode-map :states '(normal)
+            "gd" #'elisp-slime-nav-find-elisp-thing-at-point
+            "K" #'elisp-slime-nav-describe-elisp-thing-at-point)
+  :config
+  (add-hook 'emacs-lisp-mode-hook #'turn-on-elisp-slime-nav-mode))
 
 (use-package eldoc
-  :defer t
   :hook (emacs-lisp-mode . eldoc-mode)
-  :config
-  (setq eldoc-idle-delay 0.2))
+  :custom (eldoc-idle-delay 0.2))
 
 (use-package checkdoc
-  :defer t
-  :config
-  (progn
-    (setq checkdoc-force-docstrings-flag nil)
-    (setq checkdoc-arguments-in-order-flag nil)))
+  :custom
+  (checkdoc-force-docstrings-flag nil)
+  (checkdoc-arguments-in-order-flag nil))
 
 (use-package lisp-extra-font-lock
   :straight t
