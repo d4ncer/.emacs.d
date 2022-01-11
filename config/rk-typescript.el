4e6e581694158ba70ae41872e6e3b0c9d364a98d
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2022-01-12 08:59:04 +1300

    [ts] Cleanup

--- config/rk-typescript.el
+++ config/rk-typescript.el
@@ -8,7 +8,24 @@
   (require 'use-package))
 
 (require 'definers)
-(require 'rk-web-modes)
+
+;; TS + LSP (Disabled for now)
+
+(use-package typescript-mode
+  :after lsp
+  :disabled t
+  :config
+  (add-hook 'rk-ts-tsx-mode-hook #'lsp))
+
+(use-package typescript-mode
+  :straight t
+  :disabled t
+  :after lsp
+  :hook (typescript-mode . lsp)
+  :init
+  (setq lsp-clients-typescript-server-args '("--stdio" "--tsserver-log-verbosity=verbose")))
+
+;; TS + Tide (We like this)
 
 (use-package typescript-mode
   :straight t
@@ -17,12 +34,6 @@
   :init
   (define-derived-mode rk-ts-tsx-mode typescript-mode "TSX"
     "Derived mode for editing TSX files."))
-
-(use-package typescript-mode
-  :after lsp
-  :disabled t
-  :config
-  (add-hook 'rk-ts-tsx-mode-hook #'lsp))
 
 (use-package typescript-mode
   :straight t
@@ -34,16 +45,9 @@
   (dolist (lang '("node" "nodejs" "gjs" "rhino"))
     (setf (alist-get lang interpreter-mode-alist) 'typescript-mode)))
 
-(use-package typescript-mode
-  :straight t
-  :disabled t
-  :after lsp
-  :hook (typescript-mode . lsp)
-  :init
-  (setq lsp-clients-typescript-server-args '("--stdio" "--tsserver-log-verbosity=verbose")))
-
 (use-package tide
   :straight t
+  :after typescript-mode
   :commands (tide-setup)
   :custom
   (tide-server-max-response-length 999999999)
@@ -95,25 +99,24 @@
   (advice-add 'tide-project-errors :after #'rk-ts--switch-to-errors)
   (add-hook 'tide-references-mode-hook #'rk-ts--setup-tide-reference-local-binds)
   (add-hook 'tide-project-errors-mode-hook #'rk-ts--setup-tide-error-local-binds)
-  (with-eval-after-load 'typescript-mode
-    (flycheck-add-mode 'typescript-tide 'rk-ts-tsx-mode)
-    (add-hook 'typescript-mode-hook #'rk-ts--setup-tide)
-    (add-hook 'rk-ts-tsx-mode-hook #'rk-ts--setup-tide))
-
+  (add-hook 'typescript-mode-hook #'rk-ts--setup-tide)
+  (add-hook 'rk-ts-tsx-mode-hook #'rk-ts--setup-tide)
   :config
-  (with-eval-after-load 'flycheck
-    (with-eval-after-load 'rk-web-modes
-      (flycheck-add-mode 'typescript-tide 'rk-web-tsx-mode)))
-  (with-eval-after-load 'rk-web-modes
-    (rk-local-leader-def :keymaps '(typescript-mode-map rk-web-tsx-mode-map)
-      "x" '(tide-restart-server :wk "restart server")
-      "f" '(tide-fix :wk "fix")
-      "r" '(tide-rename-file :wk "rename file")
-      "i" '(tide-organize-imports :wk "organize imports")
-      "R" '(tide-refactor :wk "refactor")
-      "e" '(tide-project-errors :wk "errors")
-      "j" '(tide-jsdoc-template :wk "insert jsdoc")
-      "v" '(tide-verify-setup :wk "verify setup"))))
+  (rk-local-leader-def :keymaps '(typescript-mode-map rk-ts-tsx-mode-map)
+    "x" '(tide-restart-server :wk "restart server")
+    "f" '(tide-fix :wk "fix")
+    "r" '(tide-rename-file :wk "rename file")
+    "i" '(tide-organize-imports :wk "organize imports")
+    "R" '(tide-refactor :wk "refactor")
+    "e" '(tide-project-errors :wk "errors")
+    "j" '(tide-jsdoc-template :wk "insert jsdoc")
+    "v" '(tide-verify-setup :wk "verify setup")))
+
+(use-package tide
+  :straight t
+  :after (flycheck typescript-mode)
+  :config
+  (flycheck-add-mode 'typescript-tide 'rk-ts-tsx-mode))
 
 (use-package add-node-modules-path
   :straight t
@@ -128,6 +131,14 @@
   (add-hook 'rk-ts-tsx-mode-hook #'prettier-mode)
   (add-hook 'typescript-mode-hook #'prettier-mode))
 
+(use-package tree-sitter-langs
+  :after typescript-mode
+  :init
+  :config
+  (add-to-list 'tree-sitter-major-mode-language-alist '(rk-ts-tsx-mode . tsx)))
+
+;; LSP + Tailwind (disabled for now)
+
 (use-package lsp-tailwindcss
   :straight (:type git :host github :repo "merrickluo/lsp-tailwindcss")
   :after typescript-mode
@@ -136,11 +147,6 @@
   (setq lsp-tailwindcss-add-on-mode t)
   (setq lsp-tailwindcss-major-modes '(rk-ts-tsx-mode)))
 
-(use-package tree-sitter-langs
-  :after typescript-mode
-  :init
-  :config
-  (add-to-list 'tree-sitter-major-mode-language-alist '(rk-ts-tsx-mode . tsx)))
 
 (provide 'rk-typescript)
 
