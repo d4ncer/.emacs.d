b1e25dc7a529103fa0d8350af799de8ec6907989
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2022-01-27 19:48:57 +1300

    [org] Create migrated to migrate all people

--- lisp/roam-extensions/org-roam-migrators.el
+++ lisp/roam-extensions/org-roam-migrators.el
@@ -35,6 +35,23 @@
             (vulpea-buffer-tags-add "ca")
             (save-buffer))))))
 
+;;;###autoload
+(defun orm/person-notes ()
+  "Migrate all persons to have their names sluggified as a tag."
+  (interactive)
+  (dolist (file (org-roam-list-files))
+    (if-let* ((id (vulpea-db-get-id-by-file file))
+              (note (vulpea-db-get-by-id id))
+              (tags (vulpea-note-tags note))
+              (person-p (-contains-p tags "person"))
+              (title (vulpea-note-title note))
+              (untagged (not (-contains-p tags (rk-vulpea--person-to-tag title)))))
+        (vulpea-utils-with-note note
+          (message "Processing %s" title)
+          (goto-char (point-min))
+          (vulpea-buffer-tags-add (rk-vulpea--person-to-tag title))
+          (save-buffer)))))
+
 (provide 'org-roam-migrators)
 
 ;;; org-roam-migrators.el ends here
