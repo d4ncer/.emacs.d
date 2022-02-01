dd8f156334e2ec4f3b10498df451e92bee2b3dcc
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2022-02-02 08:55:14 +1300

    [org] Clean empty blocks in agenda

--- config/rk-org.el
+++ config/rk-org.el
@@ -272,6 +272,38 @@ Do not scheduled items or repeating todos."
     (let ((agenda-key "g"))
       (org-agenda current-prefix-arg agenda-key)
       (delete-other-windows)))
+  (defun org-agenda-delete-empty-blocks ()
+    "Remove empty agenda blocks.
+  A block is identified as empty if there are fewer than 2
+  non-empty lines in the block (excluding the line with
+  `org-agenda-block-separator' characters)."
+    (when org-agenda-compact-blocks
+      (user-error "Cannot delete empty compact blocks"))
+    (setq buffer-read-only nil)
+    (save-excursion
+      (goto-char (point-min))
+      (let* ((blank-line-re "^\\s-*$")
+             (content-line-count (if (looking-at-p blank-line-re) 0 1))
+             (start-pos (point))
+             (block-re (format "%c\\{10,\\}" org-agenda-block-separator)))
+        (while (and (not (eobp)) (forward-line))
+          (cond
+           ((looking-at-p block-re)
+            (when (< content-line-count 2)
+              (delete-region start-pos (1+ (point-at-bol))))
+            (setq start-pos (point))
+            (forward-line)
+            (setq content-line-count (if (looking-at-p blank-line-re) 0 1)))
+           ((not (looking-at-p blank-line-re))
+            (setq content-line-count (1+ content-line-count)))))
+        (when (< content-line-count 2)
+          (delete-region start-pos (point-max)))
+        (goto-char (point-min))
+        ;; The above strategy can leave a separator line at the beginning
+        ;; of the buffer.
+        (when (looking-at-p block-re)
+          (delete-region (point) (1+ (point-at-eol))))))
+    (setq buffer-read-only t))
 
   :custom
   (org-stuck-projects '("+subproject-ignore-maybe-done"
@@ -316,8 +348,7 @@ Do not scheduled items or repeating todos."
     "p" '(org-agenda-set-property :wk "set property")
     "r" '(org-agenda-refile :wk "refile"))
 
-  ;; Match projects that do not have a scheduled action or NEXT action.
-
+  (add-hook 'org-agenda-finalize-hook #'org-agenda-delete-empty-blocks)
   (add-hook 'org-agenda-finalize-hook #'org-agenda-to-appt))
 
 (use-package org-archive
