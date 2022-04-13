1576decfaa07b68b1a6c933b69215bfc6bfffe83
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2022-04-13 15:11:48 +1200

    [java] Don't use lombok for now
    Breaks other shit

--- config/rk-java.el
+++ config/rk-java.el
@@ -7,10 +7,28 @@
 (eval-when-compile
   (require 'use-package))
 
+(require 'f)
+(require 'gnus)
+
+(defvar rk-java--lombok-version "1.18.20")
+(defvar rk-java--lombok-path
+  (f-join gnus-home-directory (format ".m2/repository/org/projectlombok/lombok/%s/lombok-%s.jar" rk-java--lombok-version rk-java--lombok-version)))
+
 (use-package lsp-java
   :straight t
   :after lsp
   :config
+  (setq lsp-java-vmargs '("-XX:+UseParallelGC" "-XX:GCTimeRatio=4" "-XX:AdaptiveSizePolicyWeight=90" "-Dsun.zip.disableMemoryMapping=true" "-Xmx1G" "-Xms100m"))
+  ;; (setq lsp-java-vmargs
+  ;;       `("-noverify"
+  ;;         "-XX:+UseParallelGC"
+  ;;         "-XX:GCTimeRatio=4"
+  ;;         "-XX:AdaptiveSizePolicyWeight=90"
+  ;;         "-Dsun.zip.disableMemoryMapping=true"
+  ;;         "-Xmx1G"
+  ;;         "-Xms100m"
+  ;;         ,(format "-javaagent:%s" rk-java--lombok-path)
+  ;;         ,(format "-Xbootclasspath/a:%s" rk-java--lombok-path)))
   (add-hook 'java-mode-hook #'lsp))
 
 (provide 'rk-java)
