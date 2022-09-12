4c27fd70387390b880b2e9dde116d38cfc33f04e
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2022-09-12 13:00:08 +1200

    [comp] Use consult for comp

--- config/rk-completions.el
+++ config/rk-completions.el
@@ -17,6 +17,15 @@
 
 (use-package emacs
   :init
+  ;; Use `consult-completion-in-region' if Vertico is enabled.
+  ;; Otherwise use the default `completion--in-region' function.
+  (setq completion-in-region-function
+        (lambda (&rest args)
+          (apply (if vertico-mode
+                     #'consult-completion-in-region
+                   #'completion--in-region)
+                 args)))
+
   ;; Add prompt indicator to `completing-read-multiple'.
   ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
   (defun crm-indicator (args)
