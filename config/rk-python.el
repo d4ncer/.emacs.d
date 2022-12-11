687da58487a6156f135c86a259b187ddbfba9f0a
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2022-12-12 10:21:11 +1300

    [python] Simplify config

--- config/rk-python.el
+++ config/rk-python.el
@@ -11,98 +11,21 @@
 (eval-when-compile
   (require 'use-package))
 
-(require 'dash)
-(require 'definers)
 (require 'lsp)
-(require 'paths)
 
-(use-package python
-  :after lsp-mode
-  :general
-  (:keymaps 'python-mode-map
-            "<backspace>" nil
-            "DEL" nil)
-  :init
-  (setq lsp-pyls-plugins-pylint-enabled nil)
-  :hook (python-mode . lsp))
+(use-package python)
 
 (use-package pipenv
   :straight t
   :hook (python-mode . pipenv-mode))
 
-(use-package pyvenv
-  :straight t
-  :commands (pyvenv-activate pyvenv-deactivate pyvenv-workon)
-  :hook (python-mode . rk-py/pyvenv-activate-if-found)
-  :preface
-  (progn
-    (autoload 'projectile-project-p "projectile")
-    (autoload 'f-join "f")
-
-    (defvar rk-py/venv-names '(".env" "env" ".venv" "venv" ".virtualenv"))
-
-    (defun rk-py/directory-first-ancestor (dir pred)
-      "Search up the filesystem for the first DIR satisfying PRED.
-Return the first non-nil result of evalutating PRED."
-      (let (result)
-        (while dir
-          (pcase (funcall pred dir)
-            (`nil
-             (setq dir (f-parent dir)))
-            (res
-             (setq result res)
-             (setq dir nil))))
-        result))
-
-    (defun rk-py/find-venv-in-directory (dir)
-      (-when-let ((dir) (--keep (let ((dir (f-join dir it)))
-                                  (when (f-directory? dir)
-                                    dir))
-                                rk-py/venv-names))
-        (file-truename dir)))
-
-    (defun rk-py/pyvenv-dir ()
-      (rk-py/directory-first-ancestor default-directory
-                                      #'rk-py/find-venv-in-directory))
-
-    (defun rk-py/pyvenv-activate-if-found ()
-      (-when-let (env (rk-py/pyvenv-dir))
-        (pyvenv-activate env)
-        (message "Using pyvenv at %s" (f-abbrev env))))
-
-    (defun rk-py/pyvenv-init (env)
-      (interactive
-       (list (or (rk-py/pyvenv-dir)
-                 (f-join (read-directory-name "Project root: " nil nil t) ".env"))))
-      (when (f-dir? env)
-        (user-error "Environment already exists"))
-      (let ((reporter (make-progress-reporter "Initializing pyvenv environment...")))
-        (pcase (call-process "pyvenv" nil nil nil env)
-          (`0
-           (progress-reporter-update reporter)
-           (pyvenv-activate env)
-           (progress-reporter-done reporter))
-          (_
-           (message "%sFAILED" (aref (cdr reporter) 3)))))))
-  :config
-  (rk-local-leader-def :keymaps 'python-mode-map
-    "e" '(rk-py/pyvenv-init :wk "init pyvenv")))
-
-;; TODO: Add this back once it works, seems to be broken at the moment
-;; See https://github.com/emacs-lsp/lsp-python-ms/issues/13
-(use-package lsp-python-ms
+(use-package lsp-pyright
   :after (lsp-mode python)
-  :straight (:host github :repo "emacs-lsp/lsp-python-ms"
-                   :branch "master")
+  :straight t
   :preface
-  (defconst rk-python--lsp-ms-dir (if (equal system-type 'darwin)
-                                      (expand-file-name "~/go/src/github.com/Microsoft/python-language-server/")
-                                    (expand-file-name "~/code/python-language-server/")))
-  :custom
-  (lsp-python-ms-cache-dir (f-join paths-cache-directory ".mspyls"))
-  (lsp-python-ms-executable (if (equal system-type 'darwin)
-                                (f-join rk-python--lsp-ms-dir "output/bin/Release/osx-x64/publish/Microsoft.Python.LanguageServer")
-                              (f-join rk-python--lsp-ms-dir "output/bin/Release/linux-x64/publish/Microsoft.Python.LanguageServer"))))
+  (defun rk-python--setup-lsp ()
+    (lsp-deferred))
+  :hook (python-mode . rk-python--setup-lsp))
 
 (provide 'rk-python)
 
