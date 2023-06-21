b23617e2644bd41910442f17d08c87d2b8a49ca6
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2023-06-21 18:33:24 +1200

    [treesit] Move to inbuilt treesit
    Includes pre-compiled grammars for languages I care about.

--- config/rk-tree-sitter.el
+++ config/rk-tree-sitter.el
@@ -2,69 +2,57 @@
 
 ;;; Commentary:
 
-;; tree-sitter & evil-textobj-tree-sitter are loaded in via manual paths
-;; because I want fine-grained control over their builds. This is sub-
-;; optimal, and something to change in the future.
-;;
-;; For now, they will only load in if their src dirs are properly set
-;; in paths.
-
 ;;; Code:
 
 (eval-when-compile
   (require 'use-package))
 
-(require 'paths)
-(require 'f)
+(use-package treesit
+  :init
+  (setq treesit-language-source-alist
+        '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+          (cmake "https://github.com/uyha/tree-sitter-cmake")
+          (css "https://github.com/tree-sitter/tree-sitter-css")
+          (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+          (elixir "https://github.com/elixir-lang/tree-sitter-elixir")
+          (go "https://github.com/tree-sitter/tree-sitter-go")
+          (html "https://github.com/tree-sitter/tree-sitter-html")
+          (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+          (json "https://github.com/tree-sitter/tree-sitter-json")
+          (make "https://github.com/alemuller/tree-sitter-make")
+          (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+          (python "https://github.com/tree-sitter/tree-sitter-python")
+          (toml "https://github.com/tree-sitter/tree-sitter-toml")
+          (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+          (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+          (yaml "https://github.com/ikatyang/tree-sitter-yaml"))))
 
-(use-package tree-sitter
-  :straight t
-  :commands (global-tree-sitter-mode tree-sitter-hl-mode)
-  :config
-  (global-tree-sitter-mode 1)
-  (add-hook 'tree-sitter-mode-hook #'tree-sitter-hl-mode))
-
-(use-package tree-sitter-langs
-  :straight t
-  :after tree-sitter)
-
-(use-package expand-region
-  :straight t
-  :after tree-sitter
-  :preface
-  (defun rk-er/mark-outer-tree-sitter-node ()
-    (interactive)
-    (let* ((p (point))
-           (m (or (mark) p))
-           (beg (min p m))
-           (end (max p m))
-           (root (tsc-root-node tree-sitter-tree))
-           (node (tsc-get-descendant-for-position-range root beg end))
-           (node-beg (tsc-node-start-position node))
-           (node-end (tsc-node-end-position node)))
-      ;; Node fits the region exactly. Try its parent node instead.
-      (when (and (= beg node-beg) (= end node-end))
-        (when-let ((node (tsc-get-parent node)))
-          (setq node-beg (tsc-node-start-position node)
-                node-end (tsc-node-end-position node))))
-      (set-mark node-end)
-      (goto-char node-beg)))
-  (defun rk-tree-sitter--add-tree-sitter-expansions ()
-    (make-variable-buffer-local 'er/try-expand-list)
-    (setq er/try-expand-list (append er/try-expand-list '(rk-er/mark-outer-tree-sitter-node))))
-  :config
-  (add-hook 'tree-sitter-mode-hook #'rk-tree-sitter--add-tree-sitter-expansions))
-
-(use-package evil-textobj-tree-sitter
-  :if (f-exists-p paths-evil-textobj-src-dir)
-  :after (evil tree-sitter)
-  :general
-  (:keymaps 'evil-outer-text-objects-map
-            "f" (general-predicate-dispatch nil
-                  (bound-and-true-p tree-sitter-mode) (evil-textobj-tree-sitter-get-textobj "function.outer")))
-  (:keymaps 'evil-inner-text-objects-map
-            "f" (general-predicate-dispatch nil
-                  (bound-and-true-p tree-sitter-mode) (evil-textobj-tree-sitter-get-textobj "function.inner"))))
+;; (use-package expand-region
+;;   :straight t
+;;   :after tree-sitter
+;;   :preface
+;;   (defun rk-er/mark-outer-tree-sitter-node ()
+;;     (interactive)
+;;     (let* ((p (point))
+;;            (m (or (mark) p))
+;;            (beg (min p m))
+;;            (end (max p m))
+;;            (root (tsc-root-node tree-sitter-tree))
+;;            (node (tsc-get-descendant-for-position-range root beg end))
+;;            (node-beg (tsc-node-start-position node))
+;;            (node-end (tsc-node-end-position node)))
+;;       ;; Node fits the region exactly. Try its parent node instead.
+;;       (when (and (= beg node-beg) (= end node-end))
+;;         (when-let ((node (tsc-get-parent node)))
+;;           (setq node-beg (tsc-node-start-position node)
+;;                 node-end (tsc-node-end-position node))))
+;;       (set-mark node-end)
+;;       (goto-char node-beg)))
+;;   (defun rk-tree-sitter--add-tree-sitter-expansions ()
+;;     (make-variable-buffer-local 'er/try-expand-list)
+;;     (setq er/try-expand-list (append er/try-expand-list '(rk-er/mark-outer-tree-sitter-node))))
+;;   :config
+;;   (add-hook 'tree-sitter-mode-hook #'rk-tree-sitter--add-tree-sitter-expansions))
 
 (provide 'rk-tree-sitter)
 
