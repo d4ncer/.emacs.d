11a3d355376f7720c8eda04fc53dbb7ea86d7ca2
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2023-06-22 16:34:03 +1200

    [treesit] Remove unused config
    This functionality lives in it's own module now

--- config/rk-tree-sitter.el
+++ config/rk-tree-sitter.el
@@ -28,33 +28,6 @@
           (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
           (yaml "https://github.com/ikatyang/tree-sitter-yaml"))))
 
-;; (use-package expand-region
-;;   :straight t
-;;   :after tree-sitter
-;;   :preface
-;;   (defun rk-er/mark-outer-tree-sitter-node ()
-;;     (interactive)
-;;     (let* ((p (point))
-;;            (m (or (mark) p))
-;;            (beg (min p m))
-;;            (end (max p m))
-;;            (root (tsc-root-node tree-sitter-tree))
-;;            (node (tsc-get-descendant-for-position-range root beg end))
-;;            (node-beg (tsc-node-start-position node))
-;;            (node-end (tsc-node-end-position node)))
-;;       ;; Node fits the region exactly. Try its parent node instead.
-;;       (when (and (= beg node-beg) (= end node-end))
-;;         (when-let ((node (tsc-get-parent node)))
-;;           (setq node-beg (tsc-node-start-position node)
-;;                 node-end (tsc-node-end-position node))))
-;;       (set-mark node-end)
-;;       (goto-char node-beg)))
-;;   (defun rk-tree-sitter--add-tree-sitter-expansions ()
-;;     (make-variable-buffer-local 'er/try-expand-list)
-;;     (setq er/try-expand-list (append er/try-expand-list '(rk-er/mark-outer-tree-sitter-node))))
-;;   :config
-;;   (add-hook 'tree-sitter-mode-hook #'rk-tree-sitter--add-tree-sitter-expansions))
-
 (provide 'rk-tree-sitter)
 
 ;;; rk-tree-sitter.el ends here
