d959eb0b9f71dce1e17fc85e10684898c06a1001
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2024-04-05 17:32:44 +1300

    [init] Add early-init to support native comp

---
+++ early-init.el
@@ -0,0 +1,4 @@
+;; This is needed for native comp
+;;
+;; If native comp breaks, double check these paths
+(setenv "LIBRARY_PATH" "/opt/homebrew/opt/gcc/lib/gcc/13:/opt/homebrew/opt/libgccjit/lib/gcc/13:/opt/homebrew/opt/gcc/lib/gcc/13/gcc/aarch64-apple-darwin23/13")
