a5483cec5c652ce6accfc534c2fa62394408e42d
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2024-04-25 09:08:23 +1200

    [comp] Remove codeium config

--- config/rk-completions.el
+++ config/rk-completions.el
@@ -213,45 +213,6 @@
   (rk-leader-def
     "w s" '(bookmark-view :wk "save view")))
 
-;; Codeium
-
-(use-package codeium
-  :straight (:type git :host github :repo "Exafunction/codeium.el")
-  :preface
-  (defun rk-codeium/document/text ()
-    (buffer-substring-no-properties (max (- (point) 3000) (point-min)) (min (+ (point) 1000) (point-max))))
-  ;; if you change the text, you should also change the cursor_offset
-  ;; warning: this is measured by UTF-8 encoded bytes
-  (defun rk-codeium/document/cursor_offset ()
-    (codeium-utf8-byte-length
-     (buffer-substring-no-properties (max (- (point) 3000) (point-min)) (point))))
-  :init
-  (add-to-list 'completion-at-point-functions #'codeium-completion-at-point)
-
-  ;; codeium-completion-at-point is autoloaded, but you can
-  ;; optionally set a timer, which might speed up things as the
-  ;; codeium local language server takes ~0.2s to start up
-  (add-hook 'emacs-startup-hook
-            (lambda () (run-with-timer 0.1 nil #'codeium-init)))
-
-  :custom
-  (codeium-api-enabled
-   (lambda (api)
-     (memq api '(GetCompletions Heartbeat CancelRequest GetAuthToken RegisterUser auth-redirect AcceptCompletion))))
-  (codeium/document/text 'rk-codeium/document/text)
-  (codeium/document/cursor_offset 'rk-codeium/document/cursor_offset)
-  :config
-  ;; (setq use-dialog-box nil) ;; do not use popup boxes
-
-  ;; TODO: modeline is not working
-  ;; get codeium status in the modeline
-  ;; (setq codeium-mode-line-enable
-  ;;       (lambda (api) (not (memq api '(CancelRequest Heartbeat AcceptCompletion)))))
-  ;; (add-to-list 'mode-line-format '(:eval (car-safe codeium-mode-line)) t)
-  ;; alternatively for a more extensive mode-line
-  ;; (add-to-list 'mode-line-format '(-50 "" codeium-mode-line) t)
-  )
-
 (provide 'rk-completions)
 
 ;;; rk-completions.el ends here
