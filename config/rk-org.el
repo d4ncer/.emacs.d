2ff39b6ac0ee54162b7a76eabf736687b88f8a75
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2024-06-30 10:06:16 +1200

    [org] Use emacs to view PDFs

--- config/rk-org.el
+++ config/rk-org.el
@@ -203,6 +203,9 @@ Do not scheduled items or repeating todos."
 
   :hook ((org-mode . rk-org--setup-org))
   :config
+  ;; Teach org to open PDF file: links in emacs
+  (add-to-list 'org-file-apps '("\\.pdf\\'" . emacs))
+
   (add-hook 'org-after-todo-state-change-hook #'rk-org--set-subsequent-siblings-todo-state)
   (add-hook 'org-after-todo-state-change-hook #'rk-org--set-next-todo-state)
   (add-hook 'org-mode-hook #'auto-fill-mode)
