5cc111412455197851b434ca14313b7de36314a6
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2024-11-17 16:01:30 +1300

    [emacs] Fix path

--- early-init.el
+++ early-init.el
@@ -1,4 +1,4 @@
 ;; This is needed for native comp
 ;;
 ;; If native comp breaks, double check these paths
-(setenv "LIBRARY_PATH" "/opt/homebrew/opt/gcc/lib/gcc/13:/opt/homebrew/opt/libgccjit/lib/gcc/13:/opt/homebrew/opt/gcc/lib/gcc/13/gcc/aarch64-apple-darwin23/13")
+(setenv "LIBRARY_PATH" "/opt/homebrew/opt/gcc/lib/gcc/14:/opt/homebrew/opt/libgccjit/lib/gcc/14:/opt/homebrew/opt/gcc/lib/gcc/14/gcc/aarch64-apple-darwin23/14")
