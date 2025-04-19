8cc13fd9ea66fb284af030ddfa8797b6044130ed
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2025-04-19 16:35:12 +1200

    [help] Help & things

--- init.el
+++ init.el
@@ -2123,6 +2123,65 @@ file in your browser at the visited revision."
         (dired project-root))))
   (projectile-mode +1))
 
+;;; Documentation systems
+
+(use-package help
+  ;; Defines the main help functionality for Emacs & Emacs-Lisp.
+  :hook (help-mode-hook . turn-on-visual-line-mode)
+  :custom
+  (help-window-select t)
+  :general
+  (:keymaps 'help-map
+            "h" nil ; view-hello-file: never intended, always annoying
+            "l" #'find-library
+            "c" #'describe-face
+            "P" #'describe-text-properties))
+
+(use-package help-fns
+  :general
+  (:keymaps 'help-map
+            "K" #'describe-keymap))
+
+(use-package help-mode
+  ;; Major-mode for help buffers.
+  :general (:keymaps 'help-mode-map :states 'normal
+                     "^" #'help-go-back
+                     "M-n" #'forward-button
+                     "M-p" #'backward-button
+                     "C-n" #'forward-button
+                     "C-p" #'backward-button))
+
+(use-package eldoc
+  ;; Display help hints in the echo area as you move around.
+  :config
+  ;; Teach eldoc to re-run after these commands.
+  (eldoc-add-command 'evil-normal-state
+                     'evil-insert
+                     'evil-change
+                     'evil-delete
+                     'evil-replace))
+
+(use-package info
+  ;; Emacs' built-in system for reading texinfo manuals.
+  :general
+  (:keymaps 'help-map "s" #'info-apropos)
+  (:keymaps 'Info-mode-map :states 'normal
+            "^" #'Info-up
+            "C-n" #'Info-forward-node
+            "C-p" #'Info-backward-node))
+
+(use-package man
+  ;; Built-in manpage reader.
+  :custom
+  ;; Tell man to use pop-to-buffer under the hood, which uses display-buffer and
+  ;; selects the window.
+  (Man-notify-method 'aggressive))
+
+(use-package imenu
+  ;; Emacs' built-in navigator for points of interest in a buffer.
+  :general-config
+  (:keymaps 'Info-mode-map [remap consult-imenu] #'Info-menu))
+
 ;;; CHRIS CONFIG ABOVE
 
 (use-package highlight-thing :ensure t
