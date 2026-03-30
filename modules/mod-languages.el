8ebc45349df6ded5d759fbaec0690877d01c1530
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2026-03-30 21:35:31 +1300

    [languages] Add may-i-config-mode
    Derived mode for may-i config.lisp files with elisp font-lock.
    Sets up indent functions for defcontext and with-facts.

--- modules/mod-languages.el
+++ modules/mod-languages.el
@@ -104,6 +104,23 @@
   :bind (:map markdown-mode-map
               ("C-c C-e" . markdown-do)))
 
+;;;###autoload
+(define-derived-mode may-i-config-mode lisp-data-mode "Lisp-Data [may-i]"
+  "Major-mode for may-i configuration files."
+  (setcar font-lock-defaults
+          (append '(lisp-el-font-lock-keywords
+                    lisp-el-font-lock-keywords-1
+                    lisp-el-font-lock-keywords-2)
+                  (when (boundp 'elisp-semantic-font-lock-keywords)
+                    '(elisp-semantic-font-lock-keywords)))))
+
+;;;###autoload
+(add-to-list 'auto-mode-alist (cons (rx "/may-i/config.lisp" eos)
+                                    'may-i-config-mode))
+
+(put 'defcontext 'lisp-indent-function 1)
+(put 'with-facts 'lisp-indent-function 1)
+
 ;;; Elixir and Erlang
 
 (use-package elixir-ts-mode
